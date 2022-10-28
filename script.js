d7f97455cbc0d22cdf6740d15a875152e8adc5ea
Lucas Nardon <lucaslnardon@gmail.com>
2022-10-27 22:08:51 -0300

    Refactor algo

--- script.js
+++ script.js
@@ -17,17 +17,6 @@ selects.forEach((el, index) => {
       [parseInt(e.target.name)]
     ] = e.target.value;
   });
-  if (
-    [
-      2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35, 38, 41, 44, 47, 50, 53, 56,
-      59, 62, 65, 68, 71, 74, 77, 80,
-    ].filter((er) => index === er).length > 0
-  ) {
-    el.style.marginRight = "1.5rem";
-  }
-  if ([18, 45].filter((er) => index === er).length > 0) {
-    el.style.marginBottom = "1.5rem";
-  }
 });
 
 function isValid(board, row, col, alternative) {
@@ -35,9 +24,9 @@ function isValid(board, row, col, alternative) {
     const m = 3 * Math.floor(row / 3) + Math.floor(i / 3);
     const n = 3 * Math.floor(col / 3) + (i % 3);
     if (
-      board[row][i] == alternative ||
-      board[i][col] == alternative ||
-      board[m][n] == alternative
+      board[row][i] === alternative ||
+      board[i][col] === alternative ||
+      board[m][n] === alternative
     ) {
       return false;
     }
@@ -50,12 +39,14 @@ function solveBoard() {
     for (let j = 0; j < 9; j++) {
       if (board[i][j] == " ") {
         for (let k = 1; k <= 9; k++) {
-          if (isValid(board, i, j, k)) {
+          let stringK = k.toString();
+          if (isValid(board, i, j, stringK)) {
             selects[9 * i + j].value = k;
-            selects[9 * i + j].style.backgroundColor = "red";
-            board[i][j] = k.toString();
+            selects[9 * i + j].style.backgroundColor = "#C20A00";
+            board[i][j] = stringK;
             if (solveBoard()) {
-              selects[9 * i + j].style.backgroundColor = "green";
+              selects[9 * i + j].style.backgroundColor = "#0BE377";
+              selects[9 * i + j].style.color = "black";
               return true;
             } else {
               board[i][j] = " ";
