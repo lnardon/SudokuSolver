47b8493ee90f5d1899f92e02e7e44289c948983e
Lucas Nardon <lucaslnardon@gmail.com>
2022-10-29 22:16:14 -0300

    Add board validation function

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ let board = [
   [" ", "8", " ", " ", "3", "7", " ", " ", " "],
 ];
 
-let selects = Array.from(document.getElementsByTagName("select"));
+const feedbackElement = document.getElementsByClassName("feedback")[0];
+const selects = Array.from(document.getElementsByTagName("select"));
 
 selects.forEach((el, index) => {
   el.addEventListener("change", (e) => {
@@ -20,7 +21,7 @@ selects.forEach((el, index) => {
   });
 });
 
-function isValid(board, row, col, alternative) {
+function isAlternativeValid(board, row, col, alternative) {
   for (let i = 0; i < 9; i++) {
     const m = 3 * Math.floor(row / 3) + Math.floor(i / 3);
     const n = 3 * Math.floor(col / 3) + (i % 3);
@@ -41,7 +42,7 @@ function solveBoard() {
       if (board[i][j] === " ") {
         for (let k = 1; k <= 9; k++) {
           let stringK = k.toString();
-          if (isValid(board, i, j, stringK)) {
+          if (isAlternativeValid(board, i, j, stringK)) {
             selects[9 * i + j].value = k;
             board[i][j] = stringK;
             if (solveBoard()) {
@@ -62,15 +63,42 @@ function solveBoard() {
   return true;
 }
 
+function isBoardValid() {
+  const set = new Set();
+
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      const value = board[i][j];
+      if (value !== " ") {
+        const row = `v:${value} r:${i}`;
+        const column = `v:${value} c:${j}`;
+        const box = `v:${value} b:${Math.floor(i / 3)}, ${Math.floor(j / 3)}`;
+        if (set.has(row) || set.has(column) || set.has(box)) {
+          return false;
+        } else {
+          set.add(row);
+          set.add(column);
+          set.add(box);
+        }
+      }
+    }
+  }
+
+  return true;
+}
+
 function handleSolver() {
-  document.getElementsByClassName("feedback")[0].style.display = "none";
-  document.getElementsByClassName("solveBtn")[0].disabled = true;
-  if (!solveBoard()) {
-    document.getElementsByClassName("feedback")[0].style.display = "block";
-    document.getElementsByClassName("feedback")[0].style.animation =
+  const solveButton = document.getElementsByClassName("solveBtn")[0];
+  feedbackElement.style.display = "none";
+  solveButton.disabled = true;
+  if (isBoardValid()) {
+    solveBoard();
+  } else {
+    feedbackElement.style.display = "block";
+    feedbackElement.style.animation =
       "popup 0.5s cubic-bezier(0.215, 0.610, 0.355, 1) forwards";
   }
-  document.getElementsByClassName("solveBtn")[0].disabled = false;
+  solveButton.disabled = false;
 }
 
 function clearBoard() {
